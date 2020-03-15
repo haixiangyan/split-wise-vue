7c210bd6ee51dc8dd60cdca5cb0fc84593c77751
Haixiang <haixiang6123@gmail.com>
2020-03-15 15:58:21 -0700

    Add Icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg class="icon">
+        <use :xlink:href="`#${name}`"/>
+    </svg>
+</template>
+
+<script lang="ts">
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+  try {
+    importAll(require.context("../assets/icons", true, /\.svg$/))
+  } catch (error) {
+    console.log(error)
+  }
+
+  export default {
+    props: ['name'],
+    name: "Icon"
+  }
+</script>
+
+<style scoped lang="scss">
+.icon {
+    width: 1em;
+    height: 1em;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,28 @@
 <template>
     <div class="nav">
-        <router-link to="/money">记账</router-link>
+        <router-link to="/money">
+            <Icon name="money"></Icon>
+            记账
+        </router-link>
         |
-        <router-link to="/label">标签</router-link>
+        <router-link to="/label">
+            <Icon name="label"></Icon>
+            标签
+        </router-link>
         |
-        <router-link to="/statistics">统计</router-link>
+        <router-link to="/statistics">
+            <Icon name="chart"></Icon>
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+  import Icon from "./Icon.vue"
+
   export default {
-    name: "Nav"
+    name: "Nav",
+    components: {Icon}
   }
 </script>
 
