27597f6b6799642d2e088698906b898145c33c51
Haixiang <haixiang6123@gmail.com>
2020-03-16 01:13:50 -0700

    Update notes styles

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,7 +14,7 @@
         <div>
             <label class="notes">
                 <span class="name">备注</span>
-                <input type="text">
+                <input type="text" placeholder="在这里输入备注">
             </label>
         </div>
         <div>
@@ -77,6 +77,23 @@
                 color: #999;
                 border-bottom: 1px solid;
             }
+        }
+    }
+    .notes {
+        font-size: 14px;
+        background: #f5f5f5;
+        padding-left: 16px;
+        display: flex;
+        align-items: center;
+        .name {
+            padding-right: 16px;
+        }
+        input {
+            height: 64px;
+            flex-grow: 1;
+            background: transparent;
+            border: none;
+            padding-right: 16px;
         }
     }
 </style>
