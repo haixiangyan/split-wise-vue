7aa80c1ccd6a0c928f6998f0f9e4ff0e9512eece
Haixiang <haixiang6123@gmail.com>
2020-03-16 01:18:59 -0700

    Update types styles

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -96,4 +96,27 @@
             padding-right: 16px;
         }
     }
+    .types {
+        background: #c4c4c4;
+        display: flex;
+        text-align: center;
+        font-size: 24px;
+        > li {
+            width: 50%;
+            height: 64px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+            &.selected::after {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 100%;
+                height: 4px;
+                background: #333;
+            }
+        }
+    }
 </style>
