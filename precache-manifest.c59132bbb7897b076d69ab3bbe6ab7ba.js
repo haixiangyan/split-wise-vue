2d730b860f4f69d83120236a480f382b0dbd3ff8
Haixiang <haixiang6123@gmail.com>
2020-04-30 16:21:48 -0700

    deploy

---
+++ precache-manifest.c59132bbb7897b076d69ab3bbe6ab7ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52df1354c84738a295c3",
+    "url": "/split-wise-vue/css/app.8f1c5086.css"
+  },
+  {
+    "revision": "52afa37625e6792b6cae364cb50112b9",
+    "url": "/split-wise-vue/index.html"
+  },
+  {
+    "revision": "52df1354c84738a295c3",
+    "url": "/split-wise-vue/js/app.cd7511e7.js"
+  },
+  {
+    "revision": "85ab1f8a1e4a46956004",
+    "url": "/split-wise-vue/js/chunk-vendors.4f6be36f.js"
+  },
+  {
+    "revision": "77001443e5ccc6bb7f70ec20a6b19a8e",
+    "url": "/split-wise-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/split-wise-vue/robots.txt"
+  }
+]);
